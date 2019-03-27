00d99a7db30311c5df954476998829a2023770cf
marciks <marcinhocfa@gmail.com>
2019-03-27 14:57:17 -0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,7 @@
+# WSGI Server for Development
+# Use this during development vs. apache. Can view via [url]:8001
+# Run using virtualenv. 'env/bin/python run.py'
+from app import app
+
+app.run(host='127.0.0.1', port=5000, debug=True)
+
